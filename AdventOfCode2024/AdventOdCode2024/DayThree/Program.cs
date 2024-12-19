28e4d1cb27ac435b173fe57e816a4092aa3e96ba
Milica Maravić <m.maravic@vegait.rs>
2024-12-19 11:52:46 +0100

    Day 2 and 3

---
+++ AdventOfCode2024/AdventOdCode2024/DayThree/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.RegularExpressions;
+using static System.Int32;
+
+var input = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/input.txt");
+
+static int SolvePartOne(string input)
+{
+    var pattern = @"mul\((\d+),(\d+)\)";
+
+    var matches = Regex.Matches(input, pattern);
+
+    var result = 0;
+
+    foreach (Match match in matches)
+    {
+        var number1 = Parse(match.Groups[1].Value);
+        var number2 = Parse(match.Groups[2].Value);
+
+        result += number1 * number2;
+    }
+
+    return result;
+}
+
+static int SolvePartTwo(string input)
+{
+    var mulPattern = @"mul\((\d+),(\d+)\)";
+    var controlPattern = @"do\(\)|don't\(\)";
+    
+    var tokens = Regex.Split(input, $@"({mulPattern}|{controlPattern})");
+
+    var result = 0;
+
+    var enabled = true;
+    foreach (var token in tokens)
+    {
+        if (token == "do()")
+        {
+            enabled = true;
+        }
+        else if (token == "don't()")
+        {
+            enabled = false;
+        }
+        else if (Regex.IsMatch(token, mulPattern))
+        {
+            if (enabled)
+            {
+                var match = Regex.Match(token, mulPattern);
+                var num1 = Parse(match.Groups[1].Value);
+                var num2 = Parse(match.Groups[2].Value);
+                result += num1 * num2;
+            }
+        }
+    }
+
+    return result;
+}
+
+
+Console.WriteLine("Part one result: {0}", SolvePartOne(input));
+Console.WriteLine("Part two result: {0}", SolvePartTwo(input));
---
+++ AdventOfCode2024/AdventOdCode2024/DayTwo/Program.cs
@@ -0,0 +1,50 @@
+﻿var input = File.ReadLines(AppDomain.CurrentDomain.BaseDirectory + "/input.txt").ToList();
+
+static int GetSafeReportsCount(IEnumerable<string> input, int unsafeLevelsTolerance)
+{
+    var safeCounter = 0;
+
+    foreach (var line in input)
+    {
+        var numbers = line.Split(" ", StringSplitOptions.TrimEntries).Select(int.Parse).ToList();
+
+        var isIncreasing = numbers[1] > numbers[0];
+    
+        if (IsReportSafe(numbers, isIncreasing, unsafeLevelsTolerance))
+        {
+            safeCounter++;
+        }
+    }
+
+    return safeCounter;
+}
+
+static bool IsReportSafe(List<int> report, bool isIncreasing, int unsafeLevelsTolerance)
+{
+    var unsafeLevelsCount = 0;
+    
+    for(var i = 0; i <= report.Count - 2; i ++)
+    {
+        var difference = report[i + 1] - report[i];
+        
+        if (Math.Abs(difference) is 0 or > 3)
+        {
+            unsafeLevelsCount++;
+        }
+
+        if ((isIncreasing && difference < 0) || (!isIncreasing && difference > 0))
+        {
+            unsafeLevelsCount++;
+        }
+
+        if (unsafeLevelsCount > unsafeLevelsTolerance)
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+Console.WriteLine("Part one result: " + GetSafeReportsCount(input, 0));
+Console.WriteLine("Part one result: " + GetSafeReportsCount(input, 1));
